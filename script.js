06b9b1b9304f285f16ab435ce954b3d2a687415a
Tahmid Imran <tahmidimran1@gmail.com>
2020-05-22 16:19:23 -0500

    Complete design and functionality. Need to add exponent and keyboard.

--- script.js
+++ script.js
@@ -59,80 +59,91 @@ var container3 = document.getElementById('right-grid');
 var screen = document.getElementById('input-tab');
 
 function createMainGrid() {
-    var firstRow = ['AC', "xʸ", "C", "÷", 7, 8, 9, "x", 4, 5, 6, "-", 1, 2, 3, "+", 0, ".", , '+/-', "="];
+    var firstRow = ['AC', "xʸ", "C", "÷", 7, 8, 9, "x", 4, 5, 6, "-", 1, 2, 3, "+", 0, ".", '+/-', "="];
     firstRow.forEach(element => {
-        let newDiv = document.createElement("button");
-        newDiv.className = "num-item";
-        newDiv.id = element;
-        newDiv.innerHTML = element;
-        container2.appendChild(newDiv);
+        if (element === 'AC' || element === 'xʸ' || element === 'C') {
+            let newDiv = document.createElement("button");
+            newDiv.className = "clear-item";
+            newDiv.id = element;
+            newDiv.innerHTML = element;
+            container2.appendChild(newDiv);
+        } else if (element === '÷' || element === 'x' || element === '+' || element === '-' || element === '=') {
+            let newDiv = document.createElement("button");
+            newDiv.className = "op-item";
+            newDiv.id = element;
+            newDiv.innerHTML = element;
+            container2.appendChild(newDiv);
+        } else {
+            let newDiv = document.createElement("button");
+            newDiv.className = "num-item";
+            newDiv.id = element;
+            newDiv.innerHTML = element;
+            container2.appendChild(newDiv);
+        }
     });
 }
 
 function generateMouse() {
-    const buttons = document.querySelectorAll('.num-item');
+    const buttons = document.querySelectorAll('.num-item, .clear-item, .op-item');
     var count = 0;
-    var nums = [];
-    var ops = [];
-    var all = [];
+    var result = '0';
     var signcount = 0;
-    screen.innerHTML = '0'
+    screen.innerHTML = '0';
     buttons.forEach(button => {
         button.addEventListener('click', () => {
-            if (button.id == 'AC') {
+            if (button.id === 'AC') {
                 screen.innerHTML = "0";
                 count = 0;
-                nums = [];
-                ops = [];
-                all = [];
+                result = "0";
                 signcount = 0;
-            } else if (button.id == '+/-') {
+            } else if (button.id === '+/-') {
                 signcount++;
-                if (signcount % 2 == 1)
+                if (signcount % 2 == 1) {
                     screen.innerHTML = '-' + screen.innerHTML;
-                else
+                    result = '-' + result;
+                    count++;
+                } else {
                     screen.innerHTML = screen.innerHTML.substring(1, screen.innerHTML.length);
-                count++;
-
-            } else if (button.id == 'C') {
+                    result = result.substring(1, result.length);
+                    count--;
+                }
+            } else if (button.id === 'C') {
                 if (count != 0)
                     count--;
-                if (screen.innerHTML != '0')
+                if (screen.innerHTML != '0') {
                     screen.innerHTML = screen.innerHTML.substring(0, screen.innerHTML.length - 1);
-                if (count == 0)
+                    result = result.substring(0, result.length - 1);
+                }
+                if (count == 0) {
                     screen.innerHTML = "0";
-
-            } else if (button.id == '=') {
-                var temp = "";
-                for (let i = 0; i < all.length; i++) {
-                    temp += all[i];
+                    result = "0";
                 }
-                nums.push(parseInt(temp));
-                var result = "" + operate(nums, ops[0]);
-                screen.innerHTML = result;
+
+            } else if (button.id === '=') {
+                var temp = eval(result);
+                screen.innerHTML = temp;
+                result = '';
                 count = screen.innerHTML.length;
-                nums = [];
-                ops = [];
-                all = [];
-
             } else if (count < 14) {
-
-                if (!(button.id >= 0 && button.id <= 9)) {
-                    nums.push(parseInt(screen.innerHTML));
-                    ops.push(button.id);
-                    all = [];
-                }
-                if (screen.innerHTML == '0') {
+                if (screen.innerHTML === '0') {
                     screen.innerHTML = "";
                 }
+
+                if (!(button.id >= 0 && button.id <= 9)) {
+                    if (button.id === '÷')
+                        result += "/";
+                    else if (button.id === 'x')
+                        result += '*';
+                    else
+                        result += button.id;
+                } else if (button.id >= 0 && button.id <= 9)
+                    result += button.id;
                 screen.innerHTML += button.id;
-                if (button.id >= 0 && button.id <= 9)
-                    all.push(button.id);
                 count++;
             }
         });
     });
 }
-//pemdas function, different collor buttons, second input(for result and after operator number), add keyboard, functions, make like apple, colored background
+//different color buttons, exponential function, second input(for result and after operator number), add keyboard, functions, make like apple, colored background
 createMainGrid();
 generateMouse();
