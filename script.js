54cecc47e5787278e85fa56b4bbd0e40c63a820f
Tahmid Imran <tahmidimran1@gmail.com>
2020-05-24 21:18:19 -0500

    Need to change to calculate big numbers, fix negative numbers, and change backround.

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ var container = document.getElementById("container");
 var container2 = document.getElementById("main-grid");
 var container3 = document.getElementById("right-grid");
 var screen = document.getElementById("input-tab");
-
+var screen2 = document.getElementById("other-tab");
+const charList = "+-*/^=.0123456789";
+const ops = "+-*/^";
+var result = "";
+var count = 0;
+var exps = [];
 function createMainGrid() {
   var firstRow = [
     "AC",
-    "xʸ",
     "C",
+    "xʸ",
     "÷",
     7,
     8,
@@ -57,22 +62,24 @@ function createMainGrid() {
 
 function generateMouse() {
   const buttons = document.querySelectorAll(".num-item, .clear-item, .op-item");
-  var count = 0;
-  var result = "0";
   var signcount = 0;
   screen.innerHTML = "0";
   buttons.forEach(button => {
     button.addEventListener("click", () => {
+      if (result.charAt(0) === "0") result = result.substring(1);
       if (button.id === "AC") {
+        //all clear
         screen.innerHTML = "0";
+        screen2.innerHTML = "";
         count = 0;
-        result = "0";
+        result = "";
         signcount = 0;
       } else if (button.id === "+/-") {
+        //positive/negative
         signcount++;
         if (signcount % 2 == 1) {
+          result += "-" + screen.innerHTML;
           screen.innerHTML = "-" + screen.innerHTML;
-          result = "-" + result;
           count++;
         } else {
           screen.innerHTML = screen.innerHTML.substring(
@@ -83,35 +90,58 @@ function generateMouse() {
           count--;
         }
       } else if (button.id === "C") {
+        //backspace
         if (count != 0) count--;
-        if (screen.innerHTML != "0") {
+        if (screen.innerHTML === "Infinity") {
+          count = 0;
+          screen.innerHTML = "0";
+        }
+        if (count == 0) {
+          screen.innerHTML = "0";
+          result = screen2.innerHTML;
+        } else if (screen.innerHTML != "0") {
           screen.innerHTML = screen.innerHTML.substring(
             0,
             screen.innerHTML.length - 1
           );
           result = result.substring(0, result.length - 1);
         }
-        if (count == 0) {
-          screen.innerHTML = "0";
-          result = "0";
-        }
       } else if (button.id === "=") {
-        var temp = eval(result);
-        screen.innerHTML = temp;
-        result = "";
-        count = screen.innerHTML.length;
-      } else if (count < 11) {
-        if (screen.innerHTML === "0") {
-          screen.innerHTML = "";
-          result = "";
-        }
+        //equal
+        if (screen.innerHTML === "0") result += 0;
+        screen2.innerHTML += screen.innerHTML + " = ";
+        var temp = "" + eval(result);
 
-        if (!(button.id >= 0 && button.id <= 9)) {
-          if (button.id === "÷") result += "/";
-          else if (button.id === "x") result += "*";
-          else result += button.id;
-        } else if (button.id >= 0 && button.id <= 9) result += button.id;
-        screen.innerHTML += button.id;
+        if (temp.length >= 10) {
+          temp = temp.substring(0, 10);
+        }
+        screen.innerHTML = temp;
+        result = temp;
+        count = screen.innerHTML.length;
+      } else {
+        //any other key
+        if (screen.innerHTML === "0" || screen.innerHTML === "Infinity")
+          screen.innerHTML = "";
+
+        if (!(button.id >= 0 && button.id <= 9) && button.id != ".") {
+          if (screen.innerHTML != "") {
+            if (screen2.innerHTML.indexOf("=") != -1) {
+              screen2.innerHTML = "";
+            }
+            if (button.id === "÷") result += "/";
+            else if (button.id === "x") result += "*";
+            else if (button.id === "xʸ") result += "**";
+            else result += button.id;
+            if (button.id === "xʸ")
+              screen2.innerHTML += screen.innerHTML + " " + "^" + " ";
+            else screen2.innerHTML += screen.innerHTML + " " + button.id + " ";
+            screen.innerHTML = "0";
+            count = -1;
+          } else screen.innerHTML = "0";
+        } else if (screen.innerHTML.length < 10) {
+          result += button.id;
+          screen.innerHTML += button.id;
+        }
         count++;
       }
     });
@@ -119,12 +149,72 @@ function generateMouse() {
 }
 
 function generateKeys() {
-  //const buttons = document.querySelectorAll(".num-item, .clear-item, .op-item");
+  var signcount = 0;
+  screen.innerHTML = "0";
   document.addEventListener("keydown", e => {
-    if (e.key >= 0 && e.key <= 9) console.log(e.key);
+    if (result.charAt(0) === "0") result = result.substring(1);
+    if (e.key === "a") {
+      //all clear
+      screen.innerHTML = "0";
+      count = 0;
+      result = "";
+      signcount = 0;
+      screen2.innerHTML = "";
+    } else if (e.key === "Backspace" || e.key == "c") {
+      //backspace
+      if (count != 0) count--;
+      if (screen.innerHTML === "Infinity") {
+        count = 0;
+        screen.innerHTML = "0";
+      }
+      if (count == 0) {
+        screen.innerHTML = "0";
+        result = screen2.innerHTML;
+      } else if (screen.innerHTML != "0") {
+        screen.innerHTML = screen.innerHTML.substring(
+          0,
+          screen.innerHTML.length - 1
+        );
+        result = result.substring(0, result.length - 1);
+      }
+    } else if (e.key === "=" || e.key === "Enter") {
+      //equal
+      if (screen.innerHTML === "0") result += 0;
+      screen2.innerHTML += screen.innerHTML + " = ";
+      var temp = "" + eval(result);
+      if (temp.length >= 10) {
+        temp = temp.substring(0, 10);
+      }
+      screen.innerHTML = temp;
+      result = temp;
+      count = screen.innerHTML.length;
+    } else if (charList.indexOf(e.key) != -1) {
+      //any other key
+      if (screen.innerHTML === "0" || screen.innerHTML === "Infinity")
+        screen.innerHTML = "";
+
+      if (ops.indexOf(e.key) !== -1) {
+        if (screen.innerHTML != "") {
+          if (e.key != "^") result += e.key;
+          else result += "**";
+          if (screen2.innerHTML.indexOf("=") != -1) {
+            screen2.innerHTML = "";
+          }
+          if (e.key === "*") screen2.innerHTML += screen.innerHTML + " x ";
+          else if (e.key === "/") screen2.innerHTML += screen.innerHTML + " ÷ ";
+          else screen2.innerHTML += screen.innerHTML + " " + e.key + " ";
+          screen.innerHTML = "0";
+          count = -1;
+        } else screen.innerHTML = "0";
+      } else if (screen.innerHTML.length < 10) {
+        result += e.key;
+        screen.innerHTML += e.key;
+      }
+      count++;
+    }
   });
 }
-//exponential function, second input(for result and after operator number), add keyboard, functions, colored background, fix decimal(input and output)
+//big numbers(think about nums >= 9999999999), fix second input overflow, fix negative for keyboard, colored background,add button highlights for keyboard,
 createMainGrid();
 generateMouse();
 generateKeys();
